03274e15e0d86f655970871644c74f1d6bda8a2b
Samara Grant <samara.amanda01@gmail.com>
2020-09-25 03:48:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -103,11 +103,9 @@ class Student
 
 
   def self.first_student_in_grade_10
-    SQL 
     self.first_X_students_in_grade_10(1)
 
-    DB[:conn].execute(sql)
-  end
+    DB[:conn].execute
 
 
   def self.all_students_in_grade_X(grade)
